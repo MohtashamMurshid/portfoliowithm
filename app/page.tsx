8145d29a4c3a3f0c1e36b0ecc5304e365214373e
Mohtasham Murshid <mohtashammurshid@gmail.com>
2025-07-03 22:24:04 +0800

    Update favicon and add metadata for SEO and social sharing in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,41 @@ import Landing from "@/components/Landing";
 import Skills from "@/components/Skills";
 import GithubInfo from "@/components/GithubInfo";
 
+export const metadata = {
+  metadataBase: new URL("https://portfolio.mohtasham.dev"),
+  title: "Mohtasham Murshid Madani",
+  description: "Software Engineer, AI Researcher, and Entrepreneur",
+  keywords: [
+    "Mohtasham Murshid Madani",
+    "Software Engineer",
+    "AI Researcher",
+    "Entrepreneur",
+    "Portfolio",
+    "Developer",
+  ],
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "Mohtasham Murshid Madani",
+    description: "Software Engineer, AI Researcher, and Entrepreneur",
+    images: "@/public/images/Mohtasham.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mohtasham Murshid Madani",
+    description: "Software Engineer, AI Researcher, and Entrepreneur",
+    images: "@/public/images/Mohtasham.png",
+  },
+  alternates: {
+    canonical: "https://portfolio.mohtasham.dev",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
 export default function Home() {
   return (
     <>
