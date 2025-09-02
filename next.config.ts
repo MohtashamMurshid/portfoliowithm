2d6aec8d725545b4bf603a741b1b46b73e192783
Mohtasham Murshid <mohtashammurshid@gmail.com>
2025-09-02 20:39:46 +0530

    Remove redirects from www to non-www domain in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   trailingSlash: false,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.mohtasham.dev" }],
-        destination: "https://mohtasham.dev/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
